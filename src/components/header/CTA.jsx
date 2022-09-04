f0d9461d23ef5ebbae5b199e7975405b192e142a
KhomotsoTibane <jledoza@gmail.com>
2022-09-04 14:01:47 +0200

    social links more visable

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div className="app__header-cta">
 
-        <a href={CV} download className='app__btn'>Download CV</a>
+        <a href={CV} download className='app__btn' target="_blank" >Download CV</a>
         <a href="#contact" className='app__btn app__btn-primary'>Lets Talk</a>
     </div>
   )
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -11,7 +11,7 @@ const header = () => {
       <div className="app__container app__header-container">
         <h5>Hello I'm</h5>
         <h1>Khomotso Tibane</h1>
-        <h5 className="app__text-light">Full Stack Developer</h5>
+        <h5 className="app__text-light">FullStack Web Developer</h5>
         <CTA/>
         <SocialMedia/>
 
